b93b440f3f60fb1ba5a23b3cdb07584ff61cf6a4
Renilson Borges <133821880+devreborges@users.noreply.github.com>
2025-09-18 11:30:21 -0300

    Add files via upload

--- conversor.py
+++ conversor.py
@@ -2,22 +2,26 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
-uploaded_file = st.file_uploader("📁 Envie a base de dados em csv", type=["csv"])
+# Upload do arquivo CSV
+uploaded_file = st.file_uploader("📁 Envie a base de dados em CSV", type=["csv"])
 if uploaded_file is None:
     st.warning("Por favor, envie o arquivo base para continuar.")
     st.stop()
 
+# Leitura do CSV
 df = pd.read_csv(uploaded_file)
 st.write(df)
 
 # Função para converter o DataFrame em Excel
 def to_excel(df):
     output = BytesIO()
-    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+    # Usando openpyxl como engine para evitar erro no Streamlit Cloud
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
         df.to_excel(writer, index=False, sheet_name='Dados')
-    processed_data = output.getvalue()
-    return processed_data
+    output.seek(0)
+    return output
 
+# Gerar dados Excel
 excel_data = to_excel(df)
 
 # Botão de download
